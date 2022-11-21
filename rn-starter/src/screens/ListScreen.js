4cf4cdaf560d3cd1580ac1c58da5c360428c0b79
Matt-Chisholm <mathesonchisholm@hotmail.com>
2022-11-21 11:26:17 -0800

    flatlist fun

--- rn-starter/src/screens/ListScreen.js
+++ rn-starter/src/screens/ListScreen.js
@@ -1,10 +1,32 @@
 import { View, Text, StyleSheet } from "react-native";
 import React from "react";
+import { FlatList } from "react-native-gesture-handler";
 
 export default function ListScreen() {
+  const friends = [
+    { name: "Friend #1", age: 20 },
+    { name: "Friend #2", age: 45 },
+    { name: "Friend #3", age: 32 },
+    { name: "Friend #4", age: 27 },
+    { name: "Friend #5", age: 53 },
+    { name: "Friend #6", age: 30 },
+    { name: "Friend #7", age: 21 },
+    { name: "Friend #8", age: 19 },
+    { name: "Friend #9", age: 18 },
+  ];
+
   return (
     <View>
-      <Text>ListScreen</Text>
+      <FlatList
+        data={friends}
+        renderItem={(el) => {
+          return (
+            <Text>
+              {el.item.name} - Age {el.item.age}
+            </Text>
+          );
+        }}
+      />
     </View>
   );
 }
