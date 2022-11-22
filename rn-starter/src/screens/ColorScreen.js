d0e4708c900a2755ee42d18a992d5b61e777020f
Matt-Chisholm <mathesonchisholm@hotmail.com>
2022-11-22 10:30:53 -0800

    color screen

---
+++ rn-starter/src/screens/ColorScreen.js
@@ -0,0 +1,12 @@
+import { View, Text, StyleSheet, Button } from "react-native";
+import React from "react";
+
+export default function ColorScreen() {
+  return (
+    <View>
+      <Button title='Add a Color' />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
