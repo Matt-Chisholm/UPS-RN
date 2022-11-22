fd4ca0bdb4e95f0448fa672266586d0d746e2eb5
Matt-Chisholm <mathesonchisholm@hotmail.com>
2022-11-22 12:21:36 -0800

    reducer practice on counter

--- rn-starter/src/screens/CounterScreen.js
+++ rn-starter/src/screens/CounterScreen.js
@@ -1,15 +1,37 @@
-import { useState } from "react";
+import { useReducer } from "react";
 import { View, Text, StyleSheet, Button } from "react-native";
 import React from "react";
 
 export default function CounterScreen() {
-  const [counter, setCounter] = useState(0);
+  const [state, dispatch] = useReducer(
+    // state === { count: number }
+    // action === { type: 'increment' || 'decrement', payload: 1 }
+    (state, action) => {
+      switch (action.type) {
+        case "increment":
+          return { ...state, counter: state.counter + action.payload };
+        case "decrement":
+          return { ...state, counter: state.counter - action.payload };
+        default:
+          return state;
+      }
+    },
+    { counter: 0 }
+  );
+
+  const { counter } = state;
 
   return (
     <View>
       <Text>Current Count: {counter}</Text>
-      <Button title='Increase' onPress={() => setCounter(counter + 1)} />
-      <Button title='Decrease' onPress={() => setCounter(counter - 1)} />
+      <Button
+        title='Increase'
+        onPress={() => dispatch({ type: "increment", payload: 1 })}
+      />
+      <Button
+        title='Decrease'
+        onPress={() => dispatch({ type: "decrement", payload: 1 })}
+      />
     </View>
   );
 }
