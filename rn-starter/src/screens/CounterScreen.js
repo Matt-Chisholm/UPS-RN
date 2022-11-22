3e2b877ddf7e63a32537d88e2742cd256ed3e4b0
Matt-Chisholm <mathesonchisholm@hotmail.com>
2022-11-22 10:22:05 -0800

    counter done

--- rn-starter/src/screens/CounterScreen.js
+++ rn-starter/src/screens/CounterScreen.js
@@ -1,10 +1,15 @@
+import { useState } from "react";
 import { View, Text, StyleSheet, Button } from "react-native";
 import React from "react";
 
 export default function CounterScreen() {
+  const [counter, setCounter] = useState(0);
+
   return (
     <View>
-      <Text>Current Count: </Text>
+      <Text>Current Count: {counter}</Text>
+      <Button title='Increase' onPress={() => setCounter(counter + 1)} />
+      <Button title='Decrease' onPress={() => setCounter(counter - 1)} />
     </View>
   );
 }
