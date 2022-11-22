a9397c1ca6d899f75cbbfbfe663f7725da01782c
Matt-Chisholm <mathesonchisholm@hotmail.com>
2022-11-22 11:01:38 -0800

    all working

--- rn-starter/src/components/ColorCounter.js
+++ rn-starter/src/components/ColorCounter.js
@@ -1,14 +1,26 @@
 import { View, Text, Button, StyleSheet } from "react-native";
 import React from "react";
 
-export default function ColorCounter(props) {
+export default function ColorCounter({ color, onIncrease, onDecrease }) {
   return (
-    <View>
-      <Text>{props.color}</Text>
-      <Button title='Increase' />
-      <Button title='Decrease' />
+    <View style={styles.viewStyle}>
+      <Text>{color}</Text>
+      <Button title={`Increase ${color}`} onPress={onIncrease} />
+      <Button title={`Decrease ${color}`} onPress={onDecrease} />
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  viewStyle: {
+    borderWidth: 3,
+    borderColor: "black",
+    margin: 2,
+    padding: 5,
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    height: 100,
+  },
+});
--- rn-starter/src/screens/SquareScreen.js
+++ rn-starter/src/screens/SquareScreen.js
@@ -1,11 +1,37 @@
 import { View, Text, StyleSheet } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import ColorCounter from "../components/ColorCounter";
 
 export default function SquareScreen() {
+  const [red, setRed] = useState(0);
+  const [green, setGreen] = useState(0);
+  const [blue, setBlue] = useState(0);
+  console.log(red, green, blue);
+
   return (
     <View>
-      <ColorCounter color='Red' />
+      <ColorCounter
+        color='Red'
+        onIncrease={() => setRed(red + 100)}
+        onDecrease={() => setRed(red - 100)}
+      />
+      <ColorCounter
+        color='Green'
+        onIncrease={() => setGreen(green + 100)}
+        onDecrease={() => setGreen(green - 100)}
+      />
+      <ColorCounter
+        color='Blue'
+        onIncrease={() => setBlue(blue + 100)}
+        onDecrease={() => setBlue(blue - 100)}
+      />
+      <View
+        style={{
+          height: 150,
+          width: 150,
+          backgroundColor: `rgb(${red}, ${green}, ${blue})`,
+        }}
+      />
     </View>
   );
 }
