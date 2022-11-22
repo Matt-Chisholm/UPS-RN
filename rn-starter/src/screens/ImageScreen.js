4efa8c2c6efade9b45d8dcac956c75a1a914ee99
Matt-Chisholm <mathesonchisholm@hotmail.com>
2022-11-22 09:49:27 -0800

    image screen setup

---
+++ rn-starter/src/screens/ImageScreen.js
@@ -0,0 +1,12 @@
+import { View, Text, StyleSheet } from "react-native";
+import React from "react";
+
+export default function ImageScreen(props) {
+  return (
+    <View>
+      <Text>ImageScreen</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
